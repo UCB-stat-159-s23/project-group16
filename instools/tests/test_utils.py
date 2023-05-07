bd897d477f1d461ab170d679b77c9defef69b0b0
Avery Klauke <avery.klauke@berkeley.edu>
2023-05-07 01:10:51 +0000

    Add tests to test_model_accuracy

--- instools/tests/test_utils.py
+++ instools/tests/test_utils.py
@@ -2,12 +2,18 @@ from instools import utils as ut
 import pytest
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 
 ins = pd.read_csv('data/insurance.csv')
+
+x= ['age', 'bmi', 'children','smoker']
+y= ins['sex']
 
 def test_label_encoder():
     df = ut.label_encoder(ins, ['region'])
@@ -16,4 +22,5 @@ def test_label_encoder():
     assert df['region'].dtype != str
 
 def test_model_accuracy():
-    
+    assert type(x) is list
+    assert type(y) != float
