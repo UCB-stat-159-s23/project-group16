aa1dc4f7fd87493ff09f57f873d0aca2ae846f52
Claire Mai <claire.q.mai@gmail.com>
2023-05-07 00:38:57 +0000

    finished label encoder test

--- instools/tests/test_utils.py
+++ instools/tests/test_utils.py
@@ -2,8 +2,18 @@ from instools import utils as ut
 import pytest
 import numpy as np
 import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
 ins = pd.read_csv('data/insurance.csv')
 
-def test_data():
-    assert len(ins) == 
+def test_label_encoder():
+    df = ut.label_encoder(ins, ['region'])
+    
+    assert len(df) == 1338
+    assert df['region'].dtype != str
+
+def test_model_accuracy():
+    
